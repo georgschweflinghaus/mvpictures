87fa29ce04fa16de6908fac64b3fb8a60d93f6d1
georg <georg@schweflinghaus.de>
2022-10-15 22:37:54 +0200

    Add option to chose dst folder pattern

--- mvimages.py
+++ mvimages.py
@@ -5,21 +5,23 @@ from PIL import UnidentifiedImageError
 from DirReader import DirReader
 from ImageInfo import ImageInfo, ExifImageInfoNotFound
 
-def build_dst_dirpath(dst_dir: str, imginf: ImageInfo):
-    dst_date = imginf.get_datetime().strftime("%Y%m%d")
+
+def build_dst_dirpath(dst_dir: str, imginf: ImageInfo, format: str):
+    dst_date = imginf.get_datetime().strftime(format)
     return os.path.join(dst_dir, dst_date)
 
 def build_dst_filepath(dst_dir_path: str, imginf: ImageInfo):
     dst_filename = os.path.basename(imginf.get_filename())
     return os.path.join(dst_dir_path, dst_filename)
 
-def move_file(imginf: ImageInfo, dst_dir: str):
+def move_file(imginf: ImageInfo, dst_dir: str, format: str):
     """Move a file to a destination directory.
 
         The function will assure that directories missing will be created.
-        The image described by imginf will be moved to the """
+        The image described by imginf will be moved to the destination
+        directory which will get a name as specified by the format. """
     src_file = imginf.get_filename()
-    dst_dir_path = build_dst_dirpath(dst_dir, imginf)
+    dst_dir_path = build_dst_dirpath(dst_dir, imginf, format)
     dst_file_path = build_dst_filepath(dst_dir_path, imginf)
     assure_dir_exists(dst_dir_path)
     shutil.move(src_file, dst_file_path)
@@ -37,15 +39,16 @@ def main():
         description='Move images based on their embeded exif meta data into directories baed on day of photo taken.')
     parser.add_argument('--src', nargs='?', type=str, default='.', help='Folder with the source images to be moved. (default ".")')
     parser.add_argument('--dst', nargs=1, required=True, type=str, help='Folder where directories are created and images will be moved into.')
+    parser.add_argument('--dst_format', type=str, default="%Y-%m-%d", help='Defines how destination folder name is built from date. Default is (%%Y-%%m-%%d)')
     args = parser.parse_args()
 
     files = DirReader().read(args.src)
-    assure_dst_dir_exists(args.dst[0])
+    assure_dir_exists(args.dst[0])
     for file in files:
         try:
             imginf = ImageInfo()
             imginf.read(file)
-            move_file(imginf, args.dst[0])
+            move_file(imginf, args.dst[0], args.dst_format)
         except UnidentifiedImageError as err:
             print(f"Warning: \"{file}\" may not be an image. It will not be moved!")
         except ExifImageInfoNotFound as err:
